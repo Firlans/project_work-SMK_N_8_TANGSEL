3c65305c96288a7c741e9911c45ac055b9daf76e
Firlans <firlansyah54321@gmail.com>
2025-01-23 16:36:10 +0700

    update : mengganti nama model dan menyesuaikan fields pada table

---
+++ app/Models/ChatRoom.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChatRoom extends Model
+{
+    /** @use HasFactory<\Database\Factories\ChatFactory> */
+    use HasFactory;
+
+    protected $table = 'chat_rooms';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'id_siswa',
+        'id_guru',
+        'status',
+        'is_private'
+    ];
+
+    public function siswa()
+    {
+        return $this->belongsTo(Siswa::class, 'id_siswa', 'id');
+    }
+
+    public function guru()
+    {
+        return $this->belongsTo(Guru::class, 'id_guru', 'id');
+    }
+
+    public function chatDetails()
+    {
+        return $this->hasMany(ChatDetail::class, 'id_chat_room', 'id');
+    }
+}
