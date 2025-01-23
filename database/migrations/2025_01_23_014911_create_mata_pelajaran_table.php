95d870d6361970f2ddb518faeafff32fc34c630d
Firlans <firlansyah54321@gmail.com>
2025-01-23 15:35:33 +0700

    memperbaiki kesalahan penamaan table di migration

--- database/migrations/2025_01_23_014911_create_mata_pelajaran_table.php
+++ database/migrations/2025_01_23_014911_create_mata_pelajaran_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('orang_tuas', function (Blueprint $table) {
+        Schema::create('mata_pelajaran', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('orang_tuas');
+        Schema::dropIfExists('mata_pelajaran');
     }
 };
