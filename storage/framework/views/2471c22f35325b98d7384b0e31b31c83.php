c817683c57252b68540bd47723cb539d410dcb33
Firlans <firlansyah54321@gmail.com>
2025-02-23 19:07:19 +0700

    push hasil diskusi ke 4

---
+++ storage/framework/views/2471c22f35325b98d7384b0e31b31c83.php
@@ -0,0 +1,5 @@
+<div <?php echo e($attributes->class(['fi-dropdown-list p-1'])); ?>>
+    <?php echo e($slot); ?>
+
+</div>
+<?php /**PATH /home/mrtech/Documents/project_work-SMK_N_8_TANGSEL/vendor/filament/support/src/../resources/views/components/dropdown/list/index.blade.php ENDPATH**/ ?>
