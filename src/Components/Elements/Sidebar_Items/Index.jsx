acd07aabd60d9d614242d97577c630209b93e93d
satriaharimurtii <harimurtisatria@gmail.com>
2025-03-24 17:34:27 +0700

    remove unused components

--- src/Components/Elements/Sidebar_Items/Index.jsx
+++
@@ -1,9 +0,0 @@
-const SidebarItem = ({ text }) => {
-  return (
-    <div className="py-2 px-4 hover:bg-gray-200 rounded cursor-pointer">
-      {text}
-    </div>
-  );
-};
-
-export default SidebarItem;
