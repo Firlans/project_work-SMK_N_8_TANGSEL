d4aaf6c98f012e711c9aa694064b2c6e0387ac48
satriaharimurtii <harimurtisatria@gmail.com>
2025-05-09 16:47:05 +0700

    memperbaiki fungsi handle token expired

--- src/Pages/unauthorizedPage.jsx
+++ src/Pages/unauthorizedPage.jsx
@@ -6,7 +6,7 @@ const Unauthorized = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-3xl font-bold text-red-600">Unauthorized Access</h1>
-      <p className="text-gray-600 mt-2">Anda tidak memiliki izin untuk mengakses halaman ini.</p>
+      <p className="text-gray-600 mt-2">Token sudah expired atau Anda tidak memiliki izin untuk mengakses halaman ini.</p>
       <button
         onClick={() => navigate("/")}
         className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
--- src/protectedRoute.jsx
+++ src/protectedRoute.jsx
@@ -1,16 +1,20 @@
 import { Navigate, Outlet } from "react-router-dom";
 import axiosClient from "./axiosClient";
 import Cookies from "js-cookie";
+import { isTokenExpired } from "./utils/jwt";
 
 const ProtectedRoute = ({ allowedRoles }) => {
   const token = Cookies.get("token");
-  const userRole = Cookies.get("userRole"); // Simpan role saat login
+  const role = Cookies.get("userRole"); // Simpan role saat login
 
-  if (!token) {
-    return <Navigate to="/" replace />; // Redirect ke halaman login jika belum login
+  if (!token || isTokenExpired(token)) {
+    // Jika token tidak ada atau sudah expired, hapus token dan role dari cookies
+    Cookies.remove("token");
+    Cookies.remove("userRole");
+    return <Navigate to="/unauthorized" replace />; // Redirect ke halaman login jika belum login
   }
 
-  if (!allowedRoles.includes(userRole)) {
+  if (!allowedRoles.includes(role)) {
     return <Navigate to="/unauthorized" replace />; // Redirect jika role tidak sesuai
   }
 
