237751848363c0b5dc36d1e24b429c14995cf27f
satriaharimurtii <harimurtisatria@gmail.com>
2025-06-16 18:54:05 +0700

    Menambahkan halaman chat pribadi untuk siswa dan konselor

--- src/Pages/konselor/konselorPrivateChatPage.jsx
+++ src/Pages/konselor/konselorPrivateChatPage.jsx
@@ -1,9 +1,9 @@
 import { useParams } from "react-router-dom";
-import ChatRoomDetail from "../../Components/Fragments/Chat/ChatRoomDetail";
-import Cookies from "js-cookie";
 import KonselorLayouts from "../../Components/Layouts/KonselorLayouts";
+import Cookies from "js-cookie";
+import ChatRoomDetail from "../../Components/Fragments/Chat/ChatRoomDetail";
 
-const KonselorPublicChatPage = () => {
+const KonselorPrivateChatPage = () => {
   const { id } = useParams();
   const user = JSON.parse(Cookies.get("userPrivilege"));
   const userId = user.user_id || user.id;
@@ -11,10 +11,10 @@ const KonselorPublicChatPage = () => {
   return (
     <KonselorLayouts defaultActivePage="konseling">
       <div className="p-4 max-w-4xl mx-auto">
-        <ChatRoomDetail roomId={id} userId={userId} isPrivate={false} />
+        <ChatRoomDetail roomId={id} userId={userId} isPrivate={true} />
       </div>
     </KonselorLayouts>
   );
 };
 
-export default KonselorPublicChatPage;
+export default KonselorPrivateChatPage;
--- src/Pages/siswa/siswaPrivateChatPage.jsx
+++ src/Pages/siswa/siswaPrivateChatPage.jsx
@@ -1,9 +1,9 @@
 import { useParams } from "react-router-dom";
-import ChatRoomDetail from "../../Components/Fragments/Chat/ChatRoomDetail";
 import Cookies from "js-cookie";
 import SiswaLayout from "../../Components/Layouts/SiswaLayouts";
+import ChatRoomDetail from "../../Components/Fragments/Chat/ChatRoomDetail";
 
-const SiswaPublicChatPage = () => {
+const SiswaPrivateChatPage = () => {
   const { id } = useParams();
   const user = JSON.parse(Cookies.get("userPrivilege"));
   const userId = user.user_id || user.id;
@@ -11,10 +11,10 @@ const SiswaPublicChatPage = () => {
   return (
     <SiswaLayout defaultActivePage="bk">
       <div className="p-4 max-w-4xl mx-auto">
-        <ChatRoomDetail roomId={id} userId={userId} />
+        <ChatRoomDetail roomId={id} userId={userId} isPrivate={true} />
       </div>
     </SiswaLayout>
   );
 };
 
-export default SiswaPublicChatPage;
+export default SiswaPrivateChatPage;
